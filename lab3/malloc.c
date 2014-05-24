dc6c87238c17a57fff0ae503688957e1aba7bb7c
Erik <erik.n.back@gmail.com>
2014-05-24 13:52:53 +0200

    finnished lab3

--- lab3/malloc.c
+++ lab3/malloc.c
@@ -4,7 +4,31 @@
 #include <errno.h> 
 #include <sys/mman.h>
 
-#define NALLOC 1024                                     /* minimum #units to request */
+#ifndef STRATEGY /* if not defined use system malloc */
+#define STRATEGY 1 /* Use strategy first as default */
+#endif
+
+#define STRATEGY_FIRST 1
+#define STRATEGY_BEST 2
+
+#if STRATEGY != 0 /* Really neccessarry? */
+
+
+#if STRATEGY < 1 || STRATEGY > 2
+#error STRATEGY must contain a value over 1 and below 2
+#endif
+
+
+
+
+
+#define _GNU_SOURCE
+#define NALLOC 1024    
+
+
+
+
+                                /* minimum #units to request */
 
 typedef long Align;                                     /* for alignment to long boundary */
 
@@ -91,35 +115,117 @@ static Header *morecore(unsigned nu)
   return freep;
 }
 
-void * malloc(size_t nbytes)
-{
+void * malloc(size_t nbytes){
+
   Header *p, *prevp;
   Header * morecore(unsigned);
   unsigned nunits;
 
   if(nbytes == 0) return NULL;
 
-  nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) +1;
+  nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) +1; /*Calculate units of memory to allocate*/
 
   if((prevp = freep) == NULL) {
     base.s.ptr = freep = prevp = &base;
     base.s.size = 0;
   }
-  for(p= prevp->s.ptr;  ; prevp = p, p = p->s.ptr) {
-    if(p->s.size >= nunits) {                           /* big enough */
-      if (p->s.size == nunits)                          /* exactly */
-	prevp->s.ptr = p->s.ptr;
-      else {                                            /* allocate tail end */
-	p->s.size -= nunits;
-	p += p->s.size;
-	p->s.size = nunits;
+
+
+  /*Implements strategy first fit*/
+
+  if(STRATEGY == STRATEGY_FIRST){
+    for(p= prevp->s.ptr;  ; prevp = p, p = p->s.ptr) {
+      if(p->s.size >= nunits) {                           /* big enough */
+        if (p->s.size == nunits)                          /* exactly */
+	        prevp->s.ptr = p->s.ptr;
+        else {                                            /* allocate tail end */
+	        p->s.size -= nunits;
+	        p += p->s.size;
+	        p->s.size = nunits;
+        }
+        freep = prevp;
+        return (void *)(p+1);                             /*Return the block that fits first*/
       }
-      freep = prevp;
-      return (void *)(p+1);
+      if(p == freep)                                      /* wrapped around free list */
+        if((p = morecore(nunits)) == NULL)
+	       return NULL;                                    /* none left *t*/
     }
-    if(p == freep)                                      /* wrapped around free list */
-      if((p = morecore(nunits)) == NULL)
-	return NULL;                                    /* none left */
+  }
+
+  /*Implements strategy best fit*/
+  else if (STRATEGY == STRATEGY_BEST){
+    Header * best, * prevbest;
+
+     for(p= prevp->s.ptr;  ; prevp = p, p = p->s.ptr) {
+
+        if (p->s.size == nunits)  { /* We have a perfect fit*/
+          best = p;
+          freep = prevp;
+          break; /* Break the loop becaúse a best fit has been found*/
+        }
+
+        else if(p->s.size > nunits){ /* We have a fit but not a perfect fit*/
+          if (best == 0){/* No previous best fit*/
+              best = p;
+              bestprev = prevp;
+          } 
+          else if(best->s.size > p->s.size) { /* The size of the new block is a fit but smaller than the current best fit*/
+              best = p;
+              bestprev = prevp;
+          }
+          
+        }
+        if(p == freep) { /* Looped through the free list space */
+          if(best == NULL) { /* No best fit*/
+            if((p = morecore(nunits)) == NULL) /* Call morecore to try allocate more memory */
+                return NULL;
+          }
+
+          else {/*We had best match!!!*/
+            best->s.size -= nunits;/*Remove space from block*/
+            best += best->s.size;/*Make the best fit a header */
+            best->s.size = nunits; /* Set the size of this block to the requested amount*/
+            freep = prevp;
+            break;
+          } 
+        }
+
+     }
+
+     return (void *)(best+1);
+
+
   }
 }
 
+void *realloc(void * ptr, size_t size){
+
+    Header *newp; /* the new pointer*/
+    size_t currSize;
+
+
+    if (ptr == NULL){
+      return malloc(size);
+    }
+    else if (size == 0){
+      free(ptr);
+      return NULL;
+    }
+
+    newp = ((header *)ptr) - 1; 
+    currSize = (newp->s.size-1) * sizeof(header);
+
+    if (currSize > size){
+      currSize = size;
+    }
+    
+    newp = malloc(size); /*ALlocate new space*/
+    if(newp == NULL)
+      return NULL;
+    
+    memcpy(newp, ptr, currSize); /* Copy the old pointer to new pointer */
+    free(ptr); /* Free the old pointer*/
+
+    return newp;
+
+}
