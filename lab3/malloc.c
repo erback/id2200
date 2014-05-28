5dba2afdea81c01f81e48383616eeab9e1ce1865
Erik <erik.n.back@gmail.com>
2014-05-28 12:22:24 +0200

    new

--- lab3/malloc.c
+++ lab3/malloc.c
@@ -1,4 +1,20 @@
-#include "brk.h"
+/* KTH kursID: ID2200 - Operativsystem 
+Labb 3 - malloc
+
+Skrivet av: 
+Harald Vaksdal, haraldv@kth.se 
+Erik Bäck, erback@kth.se 
+(2014) 
+Det här biblitoket skapar de inbygga biblioteksfunktionerna malloc, free och realloc
+
+*/
+
+/*
+Malloc 
+
+*/
+
+
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -28,46 +44,51 @@
 
                                 /* minimum #units to request */
 
-typedef long Align;                                     /* for alignment to long boundary */
+typedef long Align;                                     /* Alignar block */
 
-union header {                                          /* block header */
+union header {                                          /* Struktor flr block header */
   struct {
-    union header *ptr;                                  /* next block if on free list */
-    unsigned size;                                      /* size of this block  - what unit? */
+    union header *ptr;                                  /* Pekare på nästa block */
+    unsigned size;                                      /* Storlek för specfikt block */
   } s;
-  Align x;                                              /* force alignment of blocks */
+  Align x;                                              
 };
 
 typedef union header Header;
  
-static Header base;                                     /* empty list to get started */
-static Header *freep = NULL;                            /* start of free list */
+static Header base;                                     /* Tom lista initieras */
+static Header *freep = NULL;                            /* Den fria listan initieras */
 
-/* free: put block ap in the free list */
+/* free() frigör minne och stoppas in det in den länkade listan freep. Denna kod är tagen från: 
+
+http://www.ict.kth.se/courses/ID2206/COURSELIB/malloc/malloc.c
+
+
+*/
 
 void free(void * ap)
 {
   Header *bp, *p;
 
-  if(ap == NULL) return;                                /* Nothing to do */
+  if(ap == NULL) return;                                /* Returna om inget headern är NULL */
 
-  bp = (Header *) ap - 1;                               /* point to block header */
-  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+  bp = (Header *) ap - 1;                               /* Peka på blockets header */
+  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) 
     if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-      break;                                            /* freed block at atrt or end of arena */
+      break;                                            /* frigort ett block i början eller slutet av freep */
 
-  if(bp + bp->s.size == p->s.ptr) {                     /* join to upper nb */
+  if(bp + bp->s.size == p->s.ptr) {                     /* Länkar frammåtblock till nytt block i listan*/
     bp->s.size += p->s.ptr->s.size;
     bp->s.ptr = p->s.ptr->s.ptr;
   }
   else
     bp->s.ptr = p->s.ptr;
-  if(p + p->s.size == bp) {                             /* join to lower nbr */
+  if(p + p->s.size == bp) {                             /* Länkar bakomvarande block till nytt block*/
     p->s.size += bp->s.size;
     p->s.ptr = bp->s.ptr;
   } else
     p->s.ptr = bp;
-  freep = p;
+  freep = p;  //den nya fria länkade listan
 }
 
 /* morecore: ask system for more memory */
@@ -83,6 +104,13 @@ void * endHeap(void)
 }
 #endif
 
+/* morecore() frågar efter mer minne från operativsystemet och lägger till block i den fria listan: 
+
+http://www.ict.kth.se/courses/ID2206/COURSELIB/malloc/malloc.c
+
+
+*/
+
 
 static Header *morecore(unsigned nu)
 {
@@ -97,22 +125,39 @@ static Header *morecore(unsigned nu)
     nu = NALLOC;
 #ifdef MMAP
   noPages = ((nu*sizeof(Header))-1)/getpagesize() + 1;
-  cp = mmap(__endHeap, noPages*getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  cp = mmap(__endHeap, noPages*getpagesize(), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
   nu = (noPages*getpagesize())/sizeof(Header);
   __endHeap += noPages*getpagesize();
 #else
   cp = sbrk(nu*sizeof(Header));
 #endif
-  if(cp == (void *) -1){                                  /* no space at all */
+  if(cp == (void *) -1){                                  /* Inget mer minne kunde frigöras */
     perror("failed to get more memory");
     return NULL;
   }
   up = (Header *) cp;
   up->s.size = nu;
   free((void *)(up+1));
-  return freep; 
+  return freep; //Returnerar den fria listan med mer minne som efterfrågats
 }
 
+
+/*
+malloc allokerar minne från den fria litan freep. Två algoritmers tillämpas First Fit och Best Fit:
+
+First Fit : Returnerar den första minnesplatsen som mosvarar den efterfrågade minnesallokeringen (nbytes). Detta gynnar 
+vanligtvis snabbt minnesallokering framför effektivt minnesutnyttjande eftersom det inte är givet om det allokerade blocket
+motsvarar storleken hos nbytes. 
+
+
+Best Fit: Innebär vanligtvis en mer effektiv metod för utnyttjande av den fria minnet i listan freep eftersom alogritmen
+jämför olika minnesblock till den bästa "fitten" för den efterfrågade minnesallokeringen "nbytes" har hittats och returneras därför-
+Dessvärre betyder denna mer effektiva alogoritm ur ett resursutnytjingsperspektiv att allokeringsprocessen tar längre tid då
+den alltså inte väljer första bästa plats.
+
+ 
+
+*/
 void * malloc(size_t nbytes){
 
   Header *p, *prevp;
@@ -120,7 +165,7 @@ void * malloc(size_t nbytes){
   unsigned nunits;
   if(nbytes == 0) return NULL;
 
-  nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) +1; /*Calculate units of memory to allocate*/
+  nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) +1; /*Beräknar antalet minnesenheter som skall alloekeras*/
 
   if((prevp = freep) == NULL) {
     base.s.ptr = freep = prevp = &base;
